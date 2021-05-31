b3fccde78d50b3df10e7d25af8b33b979fae8cc3
Vladexy88 <jv69986998@gmail.com>
2021-05-31 17:53:24 +0300

    Update Helper.js

--- src/pages/Helper.js
+++ src/pages/Helper.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export default class Helper extends React.Component {
-    render() {
-        return (
-            <div>
-                <a href="/auth" style={{fontSize: "1.5rem"}}>Auth</a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <a href="/auth" style={{ fontSize: '1.5rem' }}>Auth</a>
+      </div>
+    );
+  }
 }
