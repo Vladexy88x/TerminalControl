eaa41a6a5694015e83ef3f872af192ed36d49f60
Vladexy88 <jv69986998@gmail.com>
2021-05-31 17:50:01 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,40 +6,38 @@ import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Image } from "react-bootstrap";
+import { Image } from 'react-bootstrap';
 
 function Navbar(props) {
   const [sidebar, setSidebar] = useState(false);
-  
+
   const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
-          <Link to='#' className='menu-bars'>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link to='#' className='menu-bars'>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
-            <Image width="130" height="130" src={props.avatarUrl} roundedCircle></Image>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-            <div className='navbar-footer'>
-              <p >Copyright © 2020</p>
+            <Image width="130" height="130" src={props.avatarUrl} roundedCircle />
+            {SidebarData.map((item, index) => (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            ))}
+            <div className="navbar-footer">
+              <p>Copyright © 2020</p>
             </div>
           </ul>
         </nav>
