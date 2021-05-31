336c63a01da1f3962ee095ad82e28bf4b4f570c1
Vladexy88 <jv69986998@gmail.com>
2021-05-31 17:52:13 +0300

    Update Buyer.js

--- src/pages/Buyer.js
+++ src/pages/Buyer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Buyers from './Buyers';
 import Terminals from './Terminals';
@@ -6,40 +6,54 @@ import Layout from './Layout';
 import { Items } from '../data/dataTable';
 import Navbar from '../components/Navbar';
 import {
-    BrowserRouter as Router,
-    Route,
-} from "react-router-dom";
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 export default class Buyer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: Items,
-            avatarUrl: localStorage.getItem("avatarUrl")
-        }
-    }
-    render() {
-        const ids = this.props.match.params.id - 1;
-        const { items } = this.state;
-        const UserInfo = () => {
-            return (
-                <div>
-                    <h4 style={{ paddingLeft: "270px" }}>ID покупателя: {items[ids].id}</h4> 
-                    <h4 style={{ paddingLeft: "270px" }}>Имя покупателя: {items[ids].username}</h4>  
-                    <h4 style={{ paddingLeft: "270px" }}>Средний чек: {items[ids].averageCheck}</h4>
-                    <h4 style={{ paddingLeft: "270px" }}>Количество покупок: {items[ids].numberOfPurchases}</h4>
-                    <h4 style={{ paddingLeft: "270px" }}>Общая выручка: {items[ids].totalRevenues}</h4>      
-                </div>
-            )
-        }
-        return (
-            <Router>
-                <Navbar avatarUrl={this.state.avatarUrl} />
-                <Route exact path='/layout' component={Layout} />
-                <Route exact path='/terminals' component={Terminals} />
-                <Route exact path='/buyers' component={Buyers} />
-                <Route exact path='/buyer/:id' component={UserInfo} />
-            </Router>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: Items,
+      avatarUrl: localStorage.getItem('avatarUrl'),
+    };
+  }
+
+  render() {
+    const ids = this.props.match.params.id - 1;
+    const { items } = this.state;
+    const UserInfo = () => (
+      <div>
+        <h4 style={{ paddingLeft: '270px' }}>
+          ID покупателя:
+          {items[ids].id}
+        </h4>
+        <h4 style={{ paddingLeft: '270px' }}>
+          Имя покупателя:
+          {items[ids].username}
+        </h4>
+        <h4 style={{ paddingLeft: '270px' }}>
+          Средний чек:
+          {items[ids].averageCheck}
+        </h4>
+        <h4 style={{ paddingLeft: '270px' }}>
+          Количество покупок:
+          {items[ids].numberOfPurchases}
+        </h4>
+        <h4 style={{ paddingLeft: '270px' }}>
+          Общая выручка:
+          {items[ids].totalRevenues}
+        </h4>
+      </div>
+    );
+    return (
+      <Router>
+        <Navbar avatarUrl={this.state.avatarUrl} />
+        <Route exact path="/layout" component={Layout} />
+        <Route exact path="/terminals" component={Terminals} />
+        <Route exact path="/buyers" component={Buyers} />
+        <Route exact path="/buyer/:id" component={UserInfo} />
+      </Router>
+    );
+  }
 }
