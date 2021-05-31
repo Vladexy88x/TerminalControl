7ee6f12fd1d4e938c7dd4966486997c075c3cd53
Vladexy88 <jv69986998@gmail.com>
2021-05-31 17:49:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
@@ -11,24 +11,24 @@ import NoMatch from './pages/NoMatch';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
-} from "react-router-dom";
+  Switch,
+} from 'react-router-dom';
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Helper} ></Route>
-          <Route exact path="/auth" component={Auth} ></Route>
-          <Route path="/home" component={Home} ></Route>
-          <Route exact path="/buyer/:id" component={Buyer} ></Route>
-          <Route exact path='/layout' component={Layout} />
-          <Route path='/terminals' component={Terminals} />
-          <Route path='/buyers' component={Buyers} />
-          <Route component={NoMatch} status={404}/>
+          <Route exact path="/" component={Helper} />
+          <Route exact path="/auth" component={Auth} />
+          <Route path="/home" component={Home} />
+          <Route exact path="/buyer/:id" component={Buyer} />
+          <Route exact path="/layout" component={Layout} />
+          <Route path="/terminals" component={Terminals} />
+          <Route path="/buyers" component={Buyers} />
+          <Route component={NoMatch} status={404} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
