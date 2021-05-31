3127924ef176fc492b920be777eb478303d81a2c
Vladexy88 <jv69986998@gmail.com>
2021-05-31 17:38:51 +0300

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,29 +1,28 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class Layout extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        let STATUS_LOGGED = true;
-        let key = this.props.location.key;
-        if (key == undefined) {
-            STATUS_LOGGED = false;
-        }
-        this.state = {
-            loggedIn: STATUS_LOGGED
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        if (this.state.loggedIn === false) {
-            //to="/auth"
-            return <Redirect to="/auth" ></Redirect>
-        }
-        return (
-            <div>
-                <h1>Layout page</h1>
-            </div>
-        )
+    let STATUS_LOGGED = true;
+    const { key } = this.props.location;
+    if (key == undefined) {
+      STATUS_LOGGED = false;
     }
+    this.state = {
+      loggedIn: STATUS_LOGGED,
+    };
+  }
+
+  render() {
+    if (this.state.loggedIn === false) {
+      return <Redirect to="/auth" />;
+    }
+    return (
+      <div>
+        <h1>Layout page</h1>
+      </div>
+    );
+  }
 }
